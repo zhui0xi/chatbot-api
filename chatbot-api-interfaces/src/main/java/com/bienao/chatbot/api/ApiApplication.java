faf8a39e0939819bd5e4119d72da2a76d11e8964
zhui0xi <zhui0xi@qq.com>
2023-03-22 17:56:46 +0800

    bienao,feat: init ddd project

--- chatbot-api-interfaces/src/main/java/com/bienao/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/com/bienao/chatbot/api/ApiApplication.java
@@ -1,9 +1,15 @@
 package com.bienao.chatbot.api;
 
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 启动入口
+ */
+
 public class ApiApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
